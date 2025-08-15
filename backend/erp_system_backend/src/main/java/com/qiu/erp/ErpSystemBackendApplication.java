60cf28f8af182144ed24f398f0b6592dc89b0ec1
qiuxuan <2833780890@qq.com>
2025-08-15 15:53:14 +0800

    init: init backend project

---
+++ backend/erp_system_backend/src/main/java/com/qiu/erp/ErpSystemBackendApplication.java
@@ -0,0 +1,18 @@
+package com.qiu.erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Qiu
+ * @deprecated 进销存系统
+ * @date 2025/08/15 15:50
+ */
+@SpringBootApplication
+public class ErpSystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErpSystemBackendApplication.class, args);
+    }
+
+}
